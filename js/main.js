11840f869834c82d9d612a13eb433770b4f343b9
Tasen1511 <102623171+Tasen1511@users.noreply.github.com>
2022-03-29 15:46:44 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Taa-Seen.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Taa-Seen.", "Designer.", "Developer.", "Freelancer."],
       typeSpeed: 100,
       loop: true,
     });
